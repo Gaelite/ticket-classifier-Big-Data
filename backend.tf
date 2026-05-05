7bb2e379e4e71cedf6f496d5e0e46142cfad317e
gaelite <eduardogael.gz@gmail.com>
2026-05-04 18:06:56 -0600

    final final

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "ticket-classifier-bigdata-state-28d764d2"
     key            = "ticket-classifier/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "ticket-classifier-big-data-locks"
+    dynamodb_table = "ticket-classifier-bigdata-locks"
   }
 }
