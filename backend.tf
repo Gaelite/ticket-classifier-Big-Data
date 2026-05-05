f5591dc68a7a5fed85eda560fc164f88255fc21d
gaelite <eduardogael.gz@gmail.com>
2026-05-04 18:05:09 -0600

    fix: update state bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ticket-classifier-big-data-state-28d764d2"
+    bucket         = "ticket-classifier-bigdata-state-28d764d2"
     key            = "ticket-classifier/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "ticket-classifier-big-data-locks"
